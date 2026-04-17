00157ccea095bd72e6b249f2eb921faf3a992aeb
AlexanderNowakBal <alexander.nowak@baloise.be>
2026-04-17 07:49:38 +0200

    a

--- MyConsoleApp/src/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/src/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca9652ad50f5445fd9ea07d878fcd6955a158270")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c36d084bfe1d96e07f141989217538c110ffe51f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
